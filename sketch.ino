44a96bb476bd34201a9d9857a97dc7ed13accaa0
Aline Nunes <2001.alinenunes@gmail.com>
2024-08-14 18:30:06 -0300

    Add files via upload

---
+++ sketch.ino
@@ -0,0 +1,43 @@
+//Desenvolva um programa onde o usuário digita um número de entrada e o
+//dispositivo pisca o led na quantidade de vezes que foi informado.
+
+const int ledPin = 17;
+int piscar = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Hello, ESP32!");
+
+  pinMode(ledPin, OUTPUT);  //Configura o pino do LED como saída
+
+  Serial.println("Insira o número de vezes que deseja que o Led pisque: ");
+
+}
+
+void loop() {
+  //Verifica se há dados disponíveis na serial
+  if (Serial.available() > 0) {
+    piscar = Serial.parseInt();
+
+    // Verifica se o número é válido (maior que 0)
+    if (piscar > 0) {
+      Serial.print("Piscar o LED " + String(piscar) + " vezes.");
+
+      // Pisca o LED o número de vezes especificado
+      for (int i = 0; i < piscar; i++) {
+        digitalWrite(ledPin, HIGH); 
+        Serial.println("LED ON");
+        delay(500);                  
+        digitalWrite(ledPin, LOW);  
+        Serial.println("LED OFF"); 
+        delay(500);                  
+      }
+    } else {
+      // Caso o usuário insira um valor inválido
+      Serial.println("Número inválido, por favor insira um valor maior que 0!");
+    }
+    Serial.println("Digite quantas vezes deseja que o Led pisque novamente: ");
+  }
+}
+
